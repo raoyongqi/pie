8aa1c8edc87b553d742faf54f9ba7b64c5e2151e
r <1152416230@qq.com>
2024-09-09 05:46:52 -0400

    0909

---
+++ pie-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MapChart from './MapChart';
+
+function App() {
+  return (
+    <div className="App" style={{ width: '100vw', height: '100vh' }}>
+      <MapChart />
+    </div>
+  );
+}
+
+export default App;
